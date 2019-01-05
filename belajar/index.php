6cb13acd46cfe8f421cc98e51950bfa230c03b1b
ahmadqo <ahmad5qomaruddin@gmail.com>
2019-01-05 22:20:52 +0700

    First commit project

---
+++ belajar/index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Belajar - Aplikasi Order Tutor Termurah di Yogyakarta</title>	
+  <link rel="icon" type="image/png" href="img/toga.png">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <!-- <link href="img/favicon.png" rel="icon"> -->
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Roboto:100,300,400,500,700|Philosopher:400,400i,700,700i" rel="stylesheet">
+
+  <!-- Bootstrap css -->
+  <!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.theme.default.min.css" rel="stylesheet">
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/modal-video/css/modal-video.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/cssbaru.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: eStartup
+    Theme URL: https://bootstrapmade.com/estartup-bootstrap-landing-page-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body>
+
+  <header id="header" class="header header-hide">
+    <div class="container">
+
+      <div id="logo" class="pull-left">
+        <h1><a href="#hero" class="scrollto">Belajar</a></h1>
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#hero">Home</a></li>
+          <li><a href="#get-started">Tentang Kami</a></li>
+          <!-- <li><a href="#features">Keunggulan Kami</a></li> -->
+          <li><a href="#contact">Kontak Kami</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
+
+  <!--==========================
+    Hero Section
+  ============================-->
+  <section id="hero" class="wow fadeIn">
+    <div class="hero-container">
+      <h1 style="color:Coral;">Selamat Datang di Belajar</h1>
+      <h2>Aplikasi untuk memesan TUTOR atau GURU PRIVAT secara efektif dan efisien</h2>
+      <img src="img/header2.png" alt="Hero Imgs">
+      <a href="#get-started" class="btn-get-started scrollto">Get Started</a>
+      <div class="btns">
+        <a href="#"><i class="fa fa-play fa-3x"></i> Google Play</a>
+      </div>
+    </div>
+  </section><!-- #hero -->
+
+  <!--==========================
+    Get Started Section
+  ============================-->
+  <section id="get-started" class="padd-section text-center wow fadeInUp">
+
+    <div class="container">
+      <div class="section-title text-center">
+
+        <h2>Misi Aplikasi Belajar </h2>
+        <p class="separator">Aplikasi Belajar dibagun untuk mewujudkan misi:</p>
+
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-6 col-lg-4">
+          <div class="feature-block">
+            <img src="img/icon/kerja-sama.png" alt="img" class="img-fluid">
+            <p style="text-align:justify;">Menjembatani banyak tutor berkualitas untuk memperoleh pendapatan tambahan.</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4">
+          <div class="feature-block">
+            <img src="img/icon/tutor-cerdas.png" alt="img" class="img-fluid">
+            <p style="text-align:justify;">Menyediakan tutor berkualitas yang dapat membimbing dan mengarahkan para pengguna berbakat dengan metode-metode yang efektif.</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4">
+          <div class="feature-block">
+            <img src="img/icon/jembatan-tutor.png" alt="img" class="img-fluid">
+            <p style="text-align:justify;">Jembatan antara tutor dan pengguna dimana pengguna dapat menentukan waktu dan tempat untuk belajar.</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4">
+          <div class="feature-block">
+            <img src="img/icon/biaya.png" alt="img" class="img-fluid">
+            <p style="text-align:justify;">Memberikan tarif yang relevan dan terjangkau untuk para pengguna.</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4">
+          <div class="feature-block">
+            <img src="img/icon/pilihan.png" alt="img" class="img-fluid">
+            <p style="text-align:justify;">Memberikan pilihan kepada pengguna untuk memilih materi pelajaran spesifik yang dibutuhkan sehingga efektif untuk mengoptimalkan hasil belajar.</p>
+          </div>
+        </div>
+        <!-- <div class="col-md-6 col-lg-4">
+          <div class="feature-block">
+
+            <img src="img/svg/cloud.svg" alt="img" class="img-fluid">
+            <h4>introducing whatsapp</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+            <a href="#">read more</a>
+
+          </div>
+        </div> -->
+
+      </div>
+    </div>
+
+  </section>
+
+  <!--==========================
+    Features Section
+  ============================-->
+
+  <section id="features" class="padd-section text-center wow fadeInUp">
+
+    <div class="container">
+      <div class="section-title text-center">
+        <h2>Kenapa Memilih Aplikasi Belajar</h2>
+        <p class="separator">Beberapa keunggulan dari Aplikasi Belajar.</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-6 col-lg-3">
+          <div class="feature-block">
+            <img src="img/icon/jam.png" alt="img" class="img-fluid">
+            <h4>Waktu Belajar Yang Fleksibel</h4>
+            <p>Waktu belajar yang fleksibel membuat anda lebih mudah untuk belajar.</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3">
+          <div class="feature-block">
+            <img src="img/icon/biaya.jpg" alt="img" class="img-fluid">
+            <h4>Biaya Untuk Belajar Terjangkau</h4>
+            <p>Biaya untuk belajar sangat terjangkau.</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3">
+          <div class="feature-block">
+            <img src="img/icon/guru.png" alt="img" class="img-fluid">
+            <h4>Tutor Yang Profesional Dibidangnya</h4>
+            <p>Tutor pada aplikasi Belajar yang profedional dibidangnya</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3">
+          <div class="feature-block">
+            <img src="img/icon/beasiswa.png" alt="img" class="img-fluid">
+            <h4>Beasiswa Untuk Pengguna Terpilih</h4>
+            <p>Tersedia beasiswa untuk pengguna aplikasi yang terpilih.</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3" style="margin:auto;">
+          <div class="feature-block">
+            <img src="img/icon/quiz.png" alt="img" class="img-fluid">
+            <h4>Banyak Kuis Berhadiah</h4>
+            <p>Tersedia kuis-kuis berhadiah yang bisa diperoleh oleh member aplikasi.</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <!--==========================
+    Contact Section
+  ============================-->
+  <section id="contact" class="padd-section wow fadeInUp">
+
+    <div class="container">
+      <div class="section-title text-center">
+        <h2>Kontak Kami</h2>
+        <p class="separator">Kami akan membalas pesan anda melalui Email anda.</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row justify-content-center">
+
+        <div class="col-lg-3 col-md-4">
+
+          <div class="info">
+            <!-- <div>
+              <i class="fa fa-map-marker"></i>
+              <p>A108 Adam Street<br>New York, NY 535022</p>
+            </div> -->
+
+            <div class="email">
+              <i class="fa fa-envelope"></i>
+              <p>indonesiabelajar2018@gmail.com</p>
+            </div>
+
+            <div>
+              <i class="fa fa-phone"></i>
+              <p>0812 4996 531</p>
+            </div>
+          </div>
+
+          <!-- <div class="social-links">
+            <a href="https://twitter.com/BELAJAR8" class="twitter" target="_blank"><i class="fa fa-twitter"></i></a>
+            <a href="https://web.facebook.com/in.belajar?_rdc=1&_rdr" class="facebook" target="_blank"><i class="fa fa-facebook"></i></a> -->
+            <!-- <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+            <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+            <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a> -->
+          <!-- </div> -->
+
+        </div>
+
+        <div class="col-lg-5 col-md-8">
+          <div class="form">
+            <div id="sendmessage">Your message has been sent. Thank you!</div>
+            <div id="errormessage"></div>
+            <form action="" method="post" role="form" class="contactForm">
+              <div class="form-group">
+                <input type="text" name="name" class="form-control" id="name" placeholder="Nama Anda" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                <div class="validation"></div>
+              </div>
+              <div class="form-group">
+                <input type="email" class="form-control" name="email" id="email" placeholder="Email Anda" data-rule="email" data-msg="Please enter a valid email" />
+                <div class="validation"></div>
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subjek" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                <div class="validation"></div>
+              </div>
+              <div class="form-group">
+                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Pesan"></textarea>
+                <div class="validation"></div>
+              </div>
+              <div class="text-left"><button type="submit">Kirim Pesan</button></div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!-- #contact -->
+
+  <!--==========================
+    Footer
+  ============================-->
+  <?php include 'footer.html'; ?>
+
+  <?php include 'footer-link.html'; ?>
+</body>
+</html>
